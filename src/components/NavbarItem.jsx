b436288bd50a8627c53c078a7902fcd425c81b07
HamedMiri72 <hamedmiri72@gmail.com>
2025-07-15 14:12:24 +0100

    nav update

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
+import { use } from 'react';
 
 export default function NavbarItem({ title, parameter }) {
 
@@ -9,10 +10,9 @@ export default function NavbarItem({ title, parameter }) {
     const genre = searchParams.get('genre');
   return (
     <div>
-        <Link href={`/?genre=${parameter}`}
-        className={`hover:text-amber-600 font-semibold
-        ${genre === parameter ? 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg': ""}`}>
-            {title}
+
+        <Link href={`/?genre=${parameter}`} className={`hover:text-amber-500 font-semibold ${genre === parameter ? 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg' : ""}`}>
+        {title}
         </Link>
     </div>
   )
